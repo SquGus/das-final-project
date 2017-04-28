0e5d833a3bc8e35f579526658a1fbbe18d55638f
SquGus <squgus@gmail.com>
2017-04-27 23:53:25 -0500

    Adds partials

---
+++ views/todo-modal.js
@@ -0,0 +1,11 @@
+exports.getPartial =
+	'<div id="modal-todo" class="modal">'+
+		'<div class="modal-content">'+
+			'<h4 class="modal-header">Modal Header</h4>'+
+			'<p class="modal-details">A bunch of text</p>'+
+		'</div>'+
+		'<div class="modal-footer">'+
+			'<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>'+
+		'</div>'+
+	'</div>'
+;
